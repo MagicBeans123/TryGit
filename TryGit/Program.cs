7855386f5ab40b0b0988c6a49cf35b729c504fe1
LittleCowPolly <LittleCowPolly@123.com>
2023-05-29 14:36:47 +0800

    测试是否本地和远程是否同步

--- TryGit/Program.cs
+++ TryGit/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace TryGit
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace TryGit
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("is me changed?");
+            BaseMessage baseMessage = new BaseMessage();
+            
+            Console.WriteLine(baseMessage.CorrelationId().ToString());
+
         }
+    }
+
+    public class BaseMessage
+    {
+        /// <summary>
+        /// Unique Identifier used by logging
+        /// </summary>
+        protected Guid _correlationId = Guid.NewGuid();
+        public Guid CorrelationId() => _correlationId;
     }
 }
